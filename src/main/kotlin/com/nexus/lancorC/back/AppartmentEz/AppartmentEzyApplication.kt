b2c97c90a806f6b7e5ae78b41e8223b5d5a27cc0
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-12-28 22:25:30 +0530

    programs added

---
+++ src/main/kotlin/com/nexus/lancorC/back/AppartmentEz/AppartmentEzyApplication.kt
@@ -0,0 +1,11 @@
+package com.nexus.lancorC.back.AppartmentEz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppartmentEzyApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppartmentEzyApplication>(*args)
+}
---
+++ src/test/kotlin/com/nexus/lancorC/back/AppartmentEz/AppartmentEzyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nexus.lancorC.back.AppartmentEz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppartmentEzyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
