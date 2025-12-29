7f3d2277619a4212dd823bfbbd64b0739978c29c
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-12-29 18:50:22 +0530

    email otp verified 2

--- src/main/kotlin/com/nexus/lancorC/back/AppartmentEz/auth/model/OtpVerifyRequest.kt
+++ src/main/kotlin/com/nexus/lancorC/back/AppartmentEz/auth/model/OtpVerifyRequest.kt
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.Size
 
 data class OtpVerifyRequest(
 
-    @field:NotBlank(message = "Email is required")
+    @field:NotBlank(message = "Email is required!!")
     val email: String,
 
-    @field:NotBlank(message = "OTP is required")
+    @field:NotBlank(message = "OTP is required!!")
     @field:Size(min = 6, max = 6, message = "OTP must be 6 digits")
     val otp: String
 )
